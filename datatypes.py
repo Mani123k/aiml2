c3194559c4687cb41cfa2513e3b842efb723e9d8
Mani123k <96532536+Mani123k@users.noreply.github.com>
2022-08-10 18:49:44 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Rules for naming variables
+
+# In[1]:
+
+
+course="Ai and ml"
+
+
+# In[2]:
+
+
+course
+
+
+# In[3]:
+
+
+Roll.dos=2
+
+
+# In[ ]:
+
+
+
+
